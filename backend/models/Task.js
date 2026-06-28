79d87567fd53706b961d68bcc04d61762277c15e
Shashwat Gupta <guptashashwat155@gmail.com>
2026-06-28 13:59:59 +0530

    feat: create Task mongoose model

---
+++ backend/models/Task.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        description: {
+            type: String,
+            default: ""
+        },
+
+        status: {
+            type: String,
+            enum: ["Pending", "In Progress", "Completed"],
+            default: "Pending"
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
